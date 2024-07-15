04baabdc09a14971ee17e7c9797a683a5317836a
DocDoWell <dandyowl84@gmail.com>
2024-07-15 09:31:23 +0100

    Lazy Prims Algorithm

---
+++ graph/Edge.java
@@ -0,0 +1,48 @@
+package graph;
+
+public class Edge implements Comparable<Edge> {
+
+    private double weight;
+    private Vertex start;
+    private Vertex target;
+
+    public Edge(Vertex start, Vertex target, double weight) {
+        this.weight = weight;
+        this.start = start;
+        this.target = target;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Vertex getStart() {
+        return start;
+    }
+
+    public void setStart(Vertex start) {
+        this.start = start;
+    }
+
+    public Vertex getTarget() {
+        return target;
+    }
+
+    public void setTarget(Vertex target) {
+        this.target = target;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return Double.compare(this.weight, o.getWeight());
+    }
+
+    @Override
+    public String toString() {
+        return start + " -----"+weight+"-------> " + target;
+    }
+}
---
+++ graph/Vertex.java
@@ -0,0 +1,32 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+
+    private String name;
+    private List<Edge> adjacentVertices;
+
+    public Vertex(String name){
+        this.name = name;
+        this.adjacentVertices = new ArrayList<>();
+    }
+
+    public void addEdge(Edge edge){
+        adjacentVertices.add(edge);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Edge> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
